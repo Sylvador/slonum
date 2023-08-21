2763697a0213ece93c529b4b8048f9361608428d
sylvador <sylvestervstolovoy@gmail.com>
2023-08-21 18:10:42 +0300

    revert: ⏪️

--- src/utils/token-cookies.util.ts
+++ src/utils/token-cookies.util.ts
@@ -5,15 +5,8 @@ import { Tokens } from '../types';
  * Установка jwt токенов в куки
  */
 export function setTokenCookies(res: Response, tokens: Tokens, cookieOptions?: CookieOptions): void {
-  if (process.env.NODE_ENV === 'development') {
-    res.cookie('access_token', tokens.accessToken, { secure: true, domain: '.slonum.ru', ...cookieOptions });
-    res.cookie('refresh_token', tokens.refreshToken, { secure: true, httpOnly: true, domain: '.slonum.ru', ...cookieOptions });
-    res.cookie('access_token', tokens.accessToken, { secure: true, domain: 'localhost', ...cookieOptions });
-    res.cookie('refresh_token', tokens.refreshToken, { secure: true, httpOnly: true, domain: 'localhost', ...cookieOptions });
-  } else {
-    res.cookie('access_token', tokens.accessToken, { secure: true, domain: '.slonum.ru', ...cookieOptions });
-    res.cookie('refresh_token', tokens.refreshToken, { secure: true, httpOnly: true, domain: '.slonum.ru', ...cookieOptions });
-  }
+  res.cookie('access_token', tokens.accessToken, { secure: true, domain: '.slonum.ru', ...cookieOptions });
+  res.cookie('refresh_token', tokens.refreshToken, { secure: true, httpOnly: true, domain: '.slonum.ru', ...cookieOptions });
 }
 
 /**
